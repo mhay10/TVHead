20e018b24ea73985daf81b4e50f1825040764a5a
mhay10 <57298046+mhay10@users.noreply.github.com>
2023-09-07 15:40:36 -0600

    Update CompositeExpressions.ino

--- code/CompositeExpressions/CompositeExpressions.ino
+++ code/CompositeExpressions/CompositeExpressions.ino
@@ -8,6 +8,7 @@ const int pins[7] = {3, 4, 5, 6, 8, 10, 11};
 int pinStates[7];
 int pressed;
 
+// Eye positions
 const int leftx = 3, lefty = 5;
 const int rightx = 12, righty = 5;
 
@@ -17,24 +18,31 @@ int x, y;
 char exprLeft, exprRight;
 
 void setup() {
-  Serial.begin(115200);
+  // Enable logging for debug
+  if (DEBUG) Serial.begin(115200);
 
+  // Initialize pins
   for (int pin : pins) pinMode(pin, INPUT);
 
+  // Initialize screen
   win.begin(NTSC);
   win.select_font(expr8x8);
 }
 
 void loop() {
+  // Clear screen before drawing again 
   win.clear_screen();
 
+  // Get pin states
   for (int i = 0; i < 7; i++) pinStates[i] = digitalRead(pins[i]);
 
+  // Print pin states if debug enabled
   if (DEBUG) {
     for (int state : pinStates) Serial.print(String(state) + " ");
     Serial.println();
   }
-  
+
+  // Set expression based on button push
   pressed = get_pressed();
   switch (pressed) {
     case 0:
@@ -74,11 +82,13 @@ void set_expression(char left, char right) {
 }
 
 void print_eyes(char leftc, char rightc) {
+  // Print expression to the screen
   print_char(leftc, leftx, lefty);
   print_char(rightc, rightx, righty);
 }
 
 void print_char(char c, int startx, int starty) {
+  // Each expression made up of 4 tiles in square
   win.print_char(startx * 8, starty * 8, c);
   win.print_char((startx + 1) * 8, starty * 8, c + 1);
   win.print_char(startx * 8, (starty + 1) * 8, c + 2);
@@ -86,6 +96,7 @@ void print_char(char c, int startx, int starty) {
 }
 
 int get_pressed() {
+  // Get first pressed pin
   for (int i = 0; i < 7; i++)
     if (pinStates[i]) return i;
 
